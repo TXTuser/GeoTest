5e1869acde08e61b355622a4efb08eef79e29a2e
TXTuser <gimosteg666@gmail.com>
2023-11-04 15:50:04 +0300

    #end

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,10 @@ let result = document.getElementById("result");
 let next = document.getElementById("next");
 let title = document.getElementById("title");
 let reset = document.getElementById("reset");
+let timetitle = document.getElementById("timetitle");
+let activeButton;
+let timeInterval;
+let timer, time;
 let buttonblock = document.getElementById("buttonblock");
 let counter = 0;
 
@@ -26,13 +30,14 @@ let data = [
 
 function checkAnswer(event) {
   if (event.target.innerHTML === data[counter].answers[0]) {
-    console.log("YES!!!!!!!!!!");
+    // console.log("YES!!!!!!!!!!");
     event.target.style.border = "3px solid green";
-    event.target.style.pointerEvents = "none";
   } else {
+    // console.log("NO!!!!!!!!!!");
     event.target.style.border = "3px solid red";
-    buttonblock.style.pointerEvents = "none";
   }
+  buttonblock.style.pointerEvents = "none";
+  activeButton = event.target;
 }
 
 function getQuestion() {
@@ -51,4 +56,31 @@ function getQuestion() {
 }
 getQuestion();
 
-next;
+function nextQuestion() {
+  counter++;
+  buttonblock.style.pointerEvents = "auto";
+  activeButton.style.border = "3px solid transparent";
+  if (counter === data.length) {
+    timetitle.innerHTML = "Молодец! Ты прошел тест!!!!!!!!!!!!!!!!";
+  } else {
+    getQuestion();
+  }
+}
+
+function resetQuestion() {
+  counter = 0;
+  buttonblock.style.pointerEvents = "auto";
+  activeButton.style.border = "3px solid transparent";
+  getQuestion();
+}
+
+time = 5;
+timeInterval = setInterval(function () {
+  time--;
+  timetitle.innerHTML = time;
+  if (time === 0) {
+    timetitle.innerHTML =
+      "Ты проиграл. Нажми на кнопку reset, <br> чтобы начать сначала.";
+    clearInterval(timeInterval);
+  }
+}, 1000);
